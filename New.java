60554e310a89b8ee92215ba85884310d21e6ed60
timber3 <timber3@naver.com>
2021-01-21 23:53:59 +0900

    It's a New World zz

---
+++ New.java
@@ -0,0 +1,22 @@
+public class New {
+
+	public New() {
+		System.out.println("It's a New Game");
+	}
+
+	public void Hello(int a )
+	{
+		for ( int i = 0; i < a ; i++)
+		{
+			System.out.println("Hello ㅠㅠ");
+		}
+	}
+
+	New wow = new New();
+
+	wow.Hello(3);
+
+
+
+}
+
